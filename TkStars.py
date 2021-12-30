2e272f70665d88181890ff7c1859a711aa8c1a4d
haraldbeker <qualcunodue>
2021-12-30 14:00:03 +0100

    create knew sky ps

--- TkStars.py
+++ TkStars.py
@@ -3,10 +3,23 @@ from tkinter import *
 import random
 
 def rgb_hack(rgb):
+    """
+    translate rgb tuple into a colorstring for tk
+    :cvar
+    """
     return "#%02x%02x%02x" % (rgb[0],rgb[1],rgb[2])
 
 StarNum=1
 def DrawStar(w, pos, divisions, size, color):
+    """
+    Draw a single stars with following parameters. Stars are tagged so they van later by moved by tag
+    :param w:   canvas to be drawn in
+    :param pos:  where to initially appear ntuple x and y in canvas coordinates
+    :param divisions:  grid divisions of star
+    :param size:       radius of star
+    :param color:      TK color of star
+    :return:
+    """
     global StarNum
     x = np.arange(0, size, size / divisions, dtype=int)
     y = np.flip(x)
@@ -16,43 +29,62 @@ def DrawStar(w, pos, divisions, size, color):
         w.create_line(x[i] + px, py,
                       px, y[i] + py,
                       px - x[i], py,
-#                      px, y[i] + py,
                       px, py - y[i],
                       x[i] + px, py,
                       fill=color,width=1,tag="Star"+str(StarNum))
-  #      w.create_line(, px, py - y[i], fill=color,width=3,tag="Star"+str(StarNum))
-  #      w.create_line(px - x[i], py,, fill=color,width=4,tag="Star"+str(StarNum))
+
     StarNum+=1
     return
 
 def BackGround():
+    """
+    Runs in Background slightly moves the stars and reschedules it self
+    """
+
+
+
 #    print("Backgound")
     global StarNum
     for star in range(StarNum):
-        x=int(random.randint(-3,3))
-        y=int(random.randint(-3,3))
-        w.move("Star"+str(star),x,y)
-    master.after(10,BackGround)
+
+        if random.random()<0.01:
+            x = int(random.randint(-10, 10))
+            y = int(random.randint(-8, 8))
+            w.move("Star" + str(star), x, y)
+            color = [random.randint(0, 255) for i in range(3)]
+            rgb = rgb_hack(color)  # translate colors into something tk under stands
+            w.itemconfig("Star"+str(star),fill=rgb)
+
+        if random.random()<0.02:
+            color = [random.randint(0, 255) for i in range(3)]
+            rgb = rgb_hack(color)  # translate colors into something tk under stands
+            w.itemconfig("Star"+str(star),fill="black")
+
+        x = int(random.randint(-1, 1))
+        y = int(random.randint(-1, 1))
+        w.move("Star" + str(star), x, y)
+    master.after(100, BackGround)
 
 
-master = Tk()
-canvas_width = 2096
+master = Tk()  # init tk
+canvas_width = 2096     # define canvas size
 canvas_height = 1200
 
 w = Canvas(master, width=canvas_width, height=canvas_height, bg="black")
-w.pack()
+w.pack()  # create and place canvas
 uc=0
-w.create_rectangle(0,0,canvas_width,canvas_height,fill="#000020")
-for stars in range(random.randint(200, 600)):
-    xp = random.randint(0, canvas_width)
+w.create_rectangle(0,0,canvas_width,canvas_height,fill="#000020") # draw very darkblue sky to also appear on print
+for stars in range(random.randint(800, 1600)):  # create random number of stars
+    xp = random.randint(0, canvas_width)       # with random attributes
     yp = random.randint(0, canvas_height)
-    steps = random.randint(15, 25)
+    steps = random.randint(4, 10)
     size = random.randint(5, 50)
     color = [random.randint(0, 255) for i in range(3)]
-    rgb = rgb_hack(color)
+    color=[0,0,0]
+    rgb = rgb_hack(color)    # translate colors into something tk under stands
     DrawStar(w, (xp, yp), steps, size, rgb)
-    if uc%10==0: master.update()
+    if uc%10==0: master.update()   # let the stars appear gradually
     uc+=1
-master.after(10,BackGround)
-w.postscript(file="Stars.ps")
-mainloop()
+master.after(10,BackGround)      # schedule Braunian movement of stars
+w.postscript(file="Stars.ps")    # print a file as a souvenir
+mainloop()                       # tk mainloop
